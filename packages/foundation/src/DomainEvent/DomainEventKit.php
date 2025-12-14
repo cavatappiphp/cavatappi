391c8563b07a636593606e7c0faa186fc6e2d844
Evan Hildreth <me@eph.me>
2025-12-13 22:33:54 -0500

    Add DomainEventKit tests to be written

---
+++ packages/foundation/src/DomainEvent/DomainEventKit.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Cavatappi\Foundation\DomainEvent;
+
+use Cavatappi\Foundation\Factories\UuidFactory;
+use Cavatappi\Foundation\Value\ValueKit;
+use DateTimeImmutable;
+use DateTimeInterface;
+use Ramsey\Uuid\UuidInterface;
+
+trait DomainEventKit {
+	use ValueKit;
+
+	public string $type { get => static::class; }
+
+	public readonly UuidInterface $id;
+	public readonly DateTimeInterface $timestamp;
+
+	private function setTimeAndId(?UuidInterface $id, ?DateTimeInterface $timestamp): void {
+		$this->timestamp = $timestamp ?? new DateTimeImmutable();
+		$this->id = $id ?? UuidFactory::date($this->timestamp);
+	}
+}
