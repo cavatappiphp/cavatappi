61e70a450f64e089fdd38018fb8338eb3203ee9b
Evan Hildreth <me@eph.me>
2025-12-20 16:21:11 -0500

    Fix linting errors

--- packages/foundation/src/DomainEvent/DomainEventKit.php
+++ packages/foundation/src/DomainEvent/DomainEventKit.php
@@ -11,9 +11,24 @@ use Ramsey\Uuid\UuidInterface;
 trait DomainEventKit {
 	use ValueKit;
 
+	/**
+	 * Use the fully-qualified class name for the Event key.
+	 *
+	 * @var string
+	 */
 	public string $type { get => static::class; }
 
+	/**
+	 * Unique identifier for this Event.
+	 *
+	 * @var UuidInterface
+	 */
 	public readonly UuidInterface $id;
+	/**
+	 * Date and time that this Event occurred.
+	 *
+	 * @var DateTimeInterface
+	 */
 	public readonly DateTimeInterface $timestamp;
 
 	private function setIdAndTime(?UuidInterface $id, ?DateTimeInterface $timestamp): void {
